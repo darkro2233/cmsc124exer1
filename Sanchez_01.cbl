baff894cc6a5cad3631ba6521a89e59cf6442e17
Gab Sanchez <brylesanchez15@gmail.com>
2019-08-19 18:00:16 +0800

    added data div.

---
+++ Sanchez_01.cbl
@@ -0,0 +1,22 @@
+        INDENTIFICATION DIVISION.
+        PROGRAM-ID. Sanchez01.
+
+        DATA DIVISION.
+            01 students.
+        	    02 student-info OCCURS 5 TIMES.
+        		    03 fullname PIC X(30).
+        		    03 sno PIC X(30).
+        		    03 course PIC X(20).
+        		    03 contact-info.
+        			    04 mobile PIC X(11).
+        			    04 landline PIC X(8).
+        		    03 age PIC 99.
+            77 EXITED PIC 9 VALUE 0.
+            77 CTR PIC 9
+            77 MENUCHOICE PIC 9 VALUE 1.
+
+        PROCEDURE DIVISION.
+        
+        MENU.
+            DISPLAY "MENU".
+            DISPLAY "1."
