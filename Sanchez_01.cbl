050b79822dae5e17f0c453d626a6fbce8a45131c
Gab Sanchez <brylesanchez15@gmail.com>
2019-08-19 18:29:00 +0800

    working copy of data and menu

--- Sanchez_01.cbl
+++ Sanchez_01.cbl
@@ -1,29 +1,33 @@
-        INDENTIFICATION DIVISION.
-        PROGRAM-ID. Sanchez01.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Sanchez01.
 
-        DATA DIVISION.
-            01 students.
-        	    02 student-info OCCURS 5 TIMES.
-        		    03 fullname PIC X(30).
-        		    03 sno PIC X(30).
-        		    03 course PIC X(20).
-        		    03 contact-info.
-        			    04 mobile PIC X(11).
-        			    04 landline PIC X(8).
-        		    03 age PIC 99.
-            77 EXITED PIC 9 VALUE 0.
-            77 CTR PIC 9
-            77 MENUCHOICE PIC 9 VALUE 1.
+       DATA DIVISION.
+        WORKING-STORAGE SECTION.
+         01 STUDENT.
+               02 student-info.
+    		       03 fullname PIC X(30).
+    		       03 sno PIC X(30).
+    		       03 course PIC X(20).
+    		       03 contact-info.
+    		           04 mobile PIC X(11).
+    			       04 landline PIC X(8).
+    		       03 age PIC 99.
+           77 EXITED PIC 9 VALUE 0.
+           77 CTR PIC 9.
+           77 MENUCHOICE PIC 9.
 
-        PROCEDURE DIVISION.
-        
-        MENU.
-            DISPLAY "MENU".
-            DISPLAY "1. ADD STUDENT". 
-            DISPLAY "2. EDIT STUDENT INFO".
-            DISPLAY "3. DELETE STUDENT".
-            DISPLAY "4. VIEW STUDENT".
-            DISPLAY "5. VIEW ALL STUDENTS".
-            DISPLAY "6. EXIT".
-            DISPLAY "CHOICE" WITH NO ADVANCING.
-            ACCEPT MENUCHOICE.
+       PROCEDURE DIVISION.
+           PERFORM MENU.
+           STOP RUN.
+
+           MENU.
+               DISPLAY "MENU".
+               DISPLAY "1. ADD STUDENT". 
+               DISPLAY "2. EDIT STUDENT INFO".
+               DISPLAY "3. DELETE STUDENT".
+               DISPLAY "4. VIEW STUDENT".
+               DISPLAY "5. VIEW ALL STUDENTS".
+               DISPLAY "6. EXIT".
+               DISPLAY "CHOICE: " WITH NO ADVANCING.
+               ACCEPT MENUCHOICE.
+               STOP RUN.
