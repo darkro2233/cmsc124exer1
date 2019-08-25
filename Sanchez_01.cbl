88b68cb15ccd1ba2b1bc5b23b23128215c92197a
Gabsanchez <brylesanchez15@gmail.com>
2019-08-25 23:11:16 +0800

    updated add student option

--- Sanchez_01.cbl
+++ Sanchez_01.cbl
@@ -3,24 +3,27 @@
 
        DATA DIVISION.
         WORKING-STORAGE SECTION.
-         01 STUDENT.
-               02 student-info.
-    		       03 fullname PIC X(30).
-    		       03 sno PIC X(30).
-    		       03 course PIC X(20).
-    		       03 contact-info.
-    		           04 mobile PIC X(11).
-    			       04 landline PIC X(8).
-    		       03 age PIC 99.
-           77 EXITED PIC 9 VALUE 0.
+            01 student.
+                02 student-info occurs 5 times.
+    	  	        03 fullname PIC X(30).
+    		        03 sno PIC X(30).
+    		        03 course PIC X(20).
+    		        03 contact-info.
+    		            04 mobile PIC X(11).
+    		 	        04 landline PIC X(8).
+    		        03 age PIC 99.
            77 CTR PIC 9.
            77 MENUCHOICE PIC 9.
+           77 STUDCOUNT PIC 9 VALUE 0.
+           77 EDITSTUDNO PIC 9.
+
 
        PROCEDURE DIVISION.
-           PERFORM MENU.
+       
+           PERFORM MENUDISPLAY UNTIL MENUCHOICE = 6.
            STOP RUN.
 
-           MENU.
+           MENUDISPLAY.
                DISPLAY "MENU".
                DISPLAY "1. ADD STUDENT". 
                DISPLAY "2. EDIT STUDENT INFO".
@@ -30,4 +33,18 @@
                DISPLAY "6. EXIT".
                DISPLAY "CHOICE: " WITH NO ADVANCING.
                ACCEPT MENUCHOICE.
-               STOP RUN.
+               DISPLAY MENUCHOICE.
+               
+               EVALUATE MENUCHOICE
+                   WHEN 1
+                       ADD 1 TO STUDCOUNT
+                       ACCEPT fullname(STUDCOUNT)
+                       ACCEPT sno(STUDCOUNT)
+                       ACCEPT course(STUDCOUNT)
+                       ACCEPT mobile(STUDCOUNT)
+                       ACCEPT landline(STUDCOUNT)
+                       ACCEPT age(STUDCOUNT)
+                       DISPLAY "DONE"
+               END-EVALUATE.
+
+ 
